173b93b3f5e3faae742c707c907feb6a5e2860ef
Willem van der Jagt <willem.vanderjagt@shopify.com>
2016-06-01 21:41:36 -0400

    reducer

---
+++ calculator.rb
@@ -0,0 +1,65 @@
+Calculator = Module.new
+
+class Calculator::Tokenizer
+  VALID_TOKENS = %w{- + / * ( )}
+
+  def initialize(calculation)
+    @calculation = "#{calculation} "
+    @current_operand = ""
+    @tokens = []
+  end
+
+  def tokenize
+    @calculation.each_char do |char|
+      if is_numeric?(char)
+        @current_operand << char
+        next
+      end
+      append_operand
+      @tokens << char if is_valid_token?(char)
+    end
+    @tokens
+  end
+
+  private
+
+  def append_operand
+    return if @current_operand == ""
+
+    @tokens << @current_operand
+    @current_operand = ""
+  end
+
+  def is_numeric?(char)
+    char =~ /\d/
+  end
+
+  def is_valid_token?(char)
+    VALID_TOKENS.include?(char)
+  end
+end
+
+class Calculator::Reducer
+  PRECEDENCE = %w{* / + -}
+
+  def initialize(tokens)
+    @tokens = tokens.dup
+  end
+
+  def reduce
+    PRECEDENCE.each { |operator| reduce_operator(operator) }
+    @tokens.first
+  end
+
+  def reduce_operator(operator)
+    expressions = []
+    while token = @tokens.shift
+      expressions << if token == operator
+        [expressions.pop, token, @tokens.shift]
+      else
+        token
+      end
+    end
+    @tokens = expressions
+  end
+end
---
+++ calculator_test.rb
@@ -0,0 +1,139 @@
+require "minitest/autorun"
+require "./calculator"
+
+class TokenizerTest < Minitest::Test
+  def test_tokenize_simple
+    tokenizer = Calculator::Tokenizer.new("4*5")
+    expected = [
+      "4",
+      "*",
+      "5"
+    ]
+    assert_equal expected, tokenizer.tokenize
+  end
+
+  def test_tokenize_longer_operands
+    tokenizer = Calculator::Tokenizer.new("400*500")
+    expected = [
+      "400",
+      "*",
+      "500"
+    ]
+    assert_equal expected, tokenizer.tokenize
+  end
+
+  def test_tokenize_simple_with_spaces
+    tokenizer = Calculator::Tokenizer.new("4 * 5")
+    expected = [
+      "4",
+      "*",
+      "5"
+    ]
+    assert_equal expected, tokenizer.tokenize
+  end
+
+  def test_tokenize_simple_with_parens
+    tokenizer = Calculator::Tokenizer.new("(4 * 5)")
+    expected = [
+      "(",
+      "4",
+      "*",
+      "5",
+      ")",
+    ]
+    assert_equal expected, tokenizer.tokenize
+  end
+end
+
+class ReducerTest < Minitest::Test
+  def test_simple
+    tokens = %w{4 * 5 - 6}
+    expected = [
+      [
+        "4",
+        "*",
+        "5"
+      ],
+      "-",
+      "6"
+    ]
+
+    assert_equal expected, Calculator::Reducer.new(tokens).reduce
+  end
+
+  def test_simple2
+    tokens = %w{4 + 5 * 6}
+    expected = [
+      "4",
+      "+",
+      [
+        "5",
+        "*",
+        "6"
+      ]
+    ]
+    assert_equal expected, Calculator::Reducer.new(tokens).reduce
+  end
+
+  def test_simple3
+    tokens = %w{4 + 5 / 6}
+    expected = [
+      "4",
+      "+",
+      [
+        "5",
+        "/",
+        "6"
+      ]
+    ]
+    assert_equal expected, Calculator::Reducer.new(tokens).reduce
+  end
+
+  def test_multiple
+    tokens = %w{4 * 5 / 6}
+    expected = [
+      [
+        "4",
+        "*",
+        "5"
+      ],
+      "/",
+      "6"
+    ]
+    assert_equal expected, Calculator::Reducer.new(tokens).reduce
+  end
+
+  def test_complex
+    tokens = %w{5 * 4 - 4 / 9 + 1 + 2 / 2 * 6}
+    expected = [
+      [
+        "5",
+        "*",
+        "4"
+      ],
+      "-",
+      [
+        [
+          [
+            "4",
+            "/",
+            "9"
+          ],
+          "+",
+          "1"
+        ],
+        "+",
+        [
+          "2",
+          "/",
+          [
+            "2",
+            "*",
+            "6"
+          ]
+        ]
+      ]
+    ]
+    assert_equal expected, Calculator::Reducer.new(tokens).reduce
+  end
+end
